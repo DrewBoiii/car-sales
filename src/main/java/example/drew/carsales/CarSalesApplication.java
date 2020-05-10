bd27518c8bf1f3aa6fc7413ee76849fa9f07b53d
DrewBoiii <dhdhdh06@mail.ru>
2020-05-10 20:48:57 +0400

    init

---
+++ src/main/java/example/drew/carsales/CarSalesApplication.java
@@ -0,0 +1,13 @@
+package example.drew.carsales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarSalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarSalesApplication.class, args);
+	}
+
+}
